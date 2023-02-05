2f7f1abd12d64dff01705dded9d0906fd0bf11f3
JuanShotLC <38728809+JuanShotLC@users.noreply.github.com>
2023-02-05 14:40:04 -0400

    Initial commit

---
+++ lib/views/ticket_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'widgets/date_selector.dart';
+import 'widgets/header.dart';
+
+class TicketScreen extends StatelessWidget {
+  const TicketScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion(
+        value: SystemUiOverlayStyle.light,
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Color.fromARGB(255, 19, 14, 25),
+                    Color.fromARGB(255, 38, 17, 56),
+                  ],
+                ),
+              ),
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+              child: Column(
+                children: const [
+                  Spacer(),
+                  Header(),
+                  Spacer(),
+                 // Seats(),
+                  Spacer(),
+                  Dates(),
+                  Spacer(),
+                 // BottomActions(),
+                ],
+              ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
